f888f249d13f6d2171a4b6f95518453395c19bb7
Giowans <gio_tails@live.com>
2020-12-13 22:14:35 -0600

    Almost ready for Hosting

--- src/api/axiosApp.js
+++ src/api/axiosApp.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({ 
-    baseURL: `${window.location.protocol}//${window.location.hostname==='localhost'?'localhost:8080':window.location.hostname}`
+    baseURL: `${window.location.protocol}//${window.location.hostname==='localhost'?'localhost:8080':process.env.REACT_APP_API_HOST}`
 });
